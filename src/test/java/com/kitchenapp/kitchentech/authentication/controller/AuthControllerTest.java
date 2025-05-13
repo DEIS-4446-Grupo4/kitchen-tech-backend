f2eec353539d5592bde2285d30ba3d7e1135aeb3
Nicolás Zagal Vallejo <u20201c429@upc.edu.pe>
2025-05-13 14:20:24 -0500

    test(): Login

--- src/test/java/com/kitchenapp/kitchentech/authentication/controller/AuthControllerTest.java
+++ src/test/java/com/kitchenapp/kitchentech/authentication/controller/AuthControllerTest.java
@@ -1,10 +1,12 @@
 package com.kitchenapp.kitchentech.authentication.controller;
 
 import com.kitchenapp.kitchentech.authentication.model.AuthResponse;
+import com.kitchenapp.kitchentech.authentication.model.LoginRequest;
 import com.kitchenapp.kitchentech.authentication.model.RegisterRestaurantRequest;
 import com.kitchenapp.kitchentech.authentication.model.RegisterStaffUserRequest;
 import com.kitchenapp.kitchentech.authentication.service.AuthService;
 import com.kitchenapp.kitchentech.user.Enums.Role;
+import com.kitchenapp.kitchentech.user.model.Restaurant;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -75,5 +77,30 @@ class AuthControllerTest {
 
     @Test
     void loginUser() {
+        // Arrange
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setUsername("345RC");
+        loginRequest.setPassword("password123");
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setId(1L);
+        authResponse.setToken("token123");
+        authResponse.setRestaurantId(10L);
+        authResponse.setRole(Role.ADMIN.name());
+
+        AuthService authServiceMock = mock(AuthService.class);
+        when(authServiceMock.login(loginRequest)).thenReturn(authResponse);
+        AuthController authController = new AuthController(authServiceMock);
+
+        // Act
+        ResponseEntity<AuthResponse> response = authController.loginUser(loginRequest);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(1L, response.getBody().getId());
+        assertEquals("token123", response.getBody().getToken());
+        assertEquals(10L, response.getBody().getRestaurantId());
+        assertEquals(Role.ADMIN.name(), response.getBody().getRole());
     }
 }
