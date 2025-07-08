b97cec361fe1c3c6f08c1c42cb4f74628a2369f1
Nicolás Zagal Vallejo <u20201c429@upc.edu.pe>
2025-07-08 07:51:27 -0500

    fix(): CORS - KitchentechApplication

--- src/main/java/com/kitchenapp/kitchentech/KitchentechApplication.java
+++ src/main/java/com/kitchenapp/kitchentech/KitchentechApplication.java
@@ -20,7 +20,7 @@ public class KitchentechApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("https://kitchentech.netlify.app/", "https://localhost:8080")
+						.allowedOrigins("https://kitchentech.netlify.app", "https://localhost:8080")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*");
 			}
